ee183a5e12bc2d68091e0d74194af3455587fdf0
IsmaelJdz <ismael_br7@hotmail.com>
2019-01-07 19:21:40 -0600

    Promise Example

--- src/js/home.js
+++ src/js/home.js
@@ -6,3 +6,28 @@ let cambia = "@LeonidasEsteban"
 function cambiarNombre(nuevoNombre) {
   cambia = nuevoNombre
 }
+
+const getUser = new Promise(function(resolve, reject) {
+  setTimeout(function () {
+    reject('all fail');
+  }, 3000)
+});
+
+// getUser
+//   .then(function () {
+//     console.log('Todo ok');
+//   })
+//   .catch(function (msg) {
+//     console.log(msg);
+//   })
+
+Promise.all([
+  getUser,
+  getUser,
+])
+  .then(function () {
+    console.log('promise all ok');
+  })
+  .catch(function() {
+    console.log('promise all fail');
+  })
